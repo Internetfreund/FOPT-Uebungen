e417996b72c4599343e4ecff945978243e915df9
Internetfreund <mlindnerm@gmail.com>
2018-10-03 16:16:07 +0200

    kleine Verbesserung

--- FOPT-Uebungen/src/fopt/Loop1.java
+++ FOPT-Uebungen/src/fopt/Loop1.java
@@ -19,14 +19,20 @@ public class Loop1 implements Runnable
 
     public static void main(String[] args)
     {
+        //so
+        new Thread(new Loop2("Thread 1")).start();
+        new Thread(new Loop2("Thread 2")).start();
+        new Thread(new Loop2("Thread 3")).start();
+/*        //oder so
         Loop1 r1 = new Loop1("Thread 1");
-        Thread t1 = new Thread(r1);
+        Thread t1 = new Thread(r1);        
         Loop1 r2 = new Loop1("Thread 2");
         Thread t2 = new Thread(r2);
         Loop1 r3 = new Loop1("Thread 3");
         Thread t3 = new Thread(r3);
         t1.start();
         t2.start();
-        t3.start();
+        t3.start(); 
+        */
     }
 }
