84ac36a0688340ee85a8143ed02014aecf936e5f
Internetfreund <mlindnerm@gmail.com>
2018-10-03 12:20:34 +0200

    Aufgabe 2.2 funktioniert und ist fertig. Aufgabe war Ändern Sie das Beispielprogramm aus Listing 2.3 des Lehrbuchs so ab, dass sich die Run‐Methode nicht mehr in einer aus Thread abgeleiteten Klasse befindet, sondern in einer Runnable implementierenden Klasse!

---
+++ FOPT-Uebungen/src/fopt/Loop1.java
@@ -0,0 +1,32 @@
+package fopt;
+
+public class Loop1 implements Runnable
+{
+    private String myName;
+    
+    public Loop1(String name)
+    {
+        myName = name;
+    }
+    
+    public void run()
+    {
+        for(int i=1; i<=100;i++)
+        {
+            System.out.println(myName + " (" + i + ")");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Loop1 r1 = new Loop1("Thread 1");
+        Thread t1 = new Thread(r1);
+        Loop1 r2 = new Loop1("Thread 2");
+        Thread t2 = new Thread(r2);
+        Loop1 r3 = new Loop1("Thread 3");
+        Thread t3 = new Thread(r3);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
